fd718ffec2479f54fedb39af4f3960c0b1f812ed
ydajiang <diyidacheng@gmail.com>
2025-06-07 16:46:49 +0800

    fix: track无效保存帧问题

--- stream/track.go
+++ stream/track.go
@@ -10,9 +10,9 @@ type Track struct {
 	Pts           int64 // 最新的PTS
 	Dts           int64 // 最新的DTS
 	FrameDuration int   // 单帧时长, timebase和推流一致
-	Packets       collections.LinkedList[*collections.ReferenceCounter[*avformat.AVPacket]]
+	Packets       *collections.LinkedList[*collections.ReferenceCounter[*avformat.AVPacket]]
 }
 
 func NewTrack(stream *avformat.AVStream, dts, pts int64) *Track {
-	return &Track{stream, dts, pts, 0, collections.LinkedList[*collections.ReferenceCounter[*avformat.AVPacket]]{}}
+	return &Track{stream, dts, pts, 0, &collections.LinkedList[*collections.ReferenceCounter[*avformat.AVPacket]]{}}
 }
