22c20f0d92036b0740326c76a0d72a20acd2f2c7
ydajiang <diyidacheng@gmail.com>
2025-09-30 10:06:02 +0800

    chore: rtc拉流不调用GatheringCompletePromise函数

--- rtc/rtc_sink.go
+++ rtc/rtc_sink.go
@@ -88,7 +88,7 @@ func (s *Sink) StartStreaming(transStream stream.TransStream) error {
 		return err
 	}
 
-	complete := webrtc.GatheringCompletePromise(connection)
+	//complete := webrtc.GatheringCompletePromise(connection)
 	answer, err := connection.CreateAnswer(nil)
 	if err != nil {
 		return err
@@ -102,7 +102,7 @@ func (s *Sink) StartStreaming(transStream stream.TransStream) error {
 		s.cb(connection.LocalDescription().SDP)
 	}
 
-	<-complete
+	//<-complete
 	connection.OnICEConnectionStateChange(func(state webrtc.ICEConnectionState) {
 		s.state = state
 		log.Sugar.Infof("ice state: %v sink: %s source: %s", state.String(), stream.SinkID2String(s.GetID()), s.SourceID)
